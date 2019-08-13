290c30702cf0f92e2ecc46d849f632d2fbf030d0
jinyy2 <jy5031le@gmail.com>
2019-08-13 22:43:47 +0900

    algorithm

---
+++ src/greed/No_11047.java
@@ -0,0 +1,35 @@
+package greed;
+import java.util.Scanner;
+//https://www.acmicpc.net/problem/11047
+public class No_11047{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int K = sc.nextInt();
+		int[] arr = new int[n];
+		int cnt = 0;
+
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+// 	use below code instead code 13 to 15 if you want to solve it without value of coins 
+//		int a = 1;
+//		for(int i=0;i<n;i++) {
+//			if(i%2==0) {
+//				arr[i] = a;
+//				a *= 5;
+// 			} else {
+//				arr[i] = a;
+//				a *= 2;
+//			}
+//		}
+		
+		for(int j=n-1;j>=0;j--) {
+				cnt += K/arr[j];
+				K %= arr[j];
+		}
+		System.out.println(cnt);
+	}
+}
