bac6e9542da6c1ccae3e4cfb449a3355da9988c9
jinyy2 <jy5031le@gmail.com>
2019-08-17 13:11:17 +0900

    algorithm

---
+++ src/greed/No_2217.java
@@ -0,0 +1,26 @@
+package greed;
+import java.util.Scanner;
+import java.util.Arrays;
+public class No_2217 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		int max = 0;
+		for(int i=0;i<n;i++) {
+			if(max<arr[i]*(n-i)) {
+				max = arr[i]*(n-i);
+			}
+		}
+		System.out.println(max);
+		sc.close();
+	}
+}
