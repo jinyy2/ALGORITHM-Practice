1297da281c891a28918d7494338a5b76408a8c1b
jinyy2 <jy5031le@gmail.com>
2019-08-31 14:10:01 +0900

    algorithm

---
+++ src/string/No2743.java
@@ -0,0 +1,12 @@
+package string;
+import java.util.Scanner;
+//https://www.acmicpc.net/problem/2743
+public class No2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(str.length());
+		sc.close();
+	}
+}
